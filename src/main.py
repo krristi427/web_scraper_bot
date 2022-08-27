3fd5a6b28233aae0f37a00da93d5a8e1fda3a188
kristi <s6krball@uni-bonn.de>
2022-08-27 20:15:39 +0200

    added help functionality

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ def unknown(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I didn't understand that command, type /help for help")
 
 def help(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=(update.effective_chat.id, text="bruh fuck off")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="bruh fuck off")
 
 
 if __name__ == '__main__':
